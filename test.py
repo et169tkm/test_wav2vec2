217299799ad3626d9b0c9ba928704dae710ad1c0
Eric Tang <github-public@erictang.com>
2022-10-16 19:10:49 +0100

    [WIP] attempt to split the audio

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import torch
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import io
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 from os import path
 
@@ -13,19 +14,42 @@ tokenizer = Wav2Vec2Processor.from_pretrained(MODEL)
 model = Wav2Vec2ForCTC.from_pretrained(MODEL)
 
 r = sr.Recognizer()
-for i in range(6):
-  print("Going to process file %d" % i)
-  audio_file = path.join(path.dirname(path.realpath(__file__)), "data/test%02d.wav" % (i+1))
-  with sr.AudioFile(audio_file) as source:
-    audio = r.record(source)
+print("Loading file")
+audio_file = AudioSegment.from_wav("data/test_long.wav")
+chunks = split_on_silence(audio_file, min_silence_len=300, silence_thresh=-16)
+print(type(chunks))
+print(type(chunks[0]))
+print(len(chunks))
 
-    data = io.BytesIO(audio.get_wav_data()) # list of bytes
-    clip = AudioSegment.from_file(data) # numpy array
-    x = torch.FloatTensor(clip.get_array_of_samples()) # tensor
+i = 0
+for chunk in chunks:
+  chunk.export("temp/%04d.mp3" % i, format="mp3")
+  i += 1
 
-    inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt', padding='longest').input_values
-    logits = model(inputs).logits
-    tokens = torch.argmax(logits, axis=-1)
-    text = tokenizer.batch_decode(tokens) # convert tokens to string
+  clip = chunk # numpy array
+  x = torch.FloatTensor(clip.get_array_of_samples()) # tensor
 
-    print("text: %s" % text)
+  inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt', padding='longest').input_values
+  logits = model(inputs).logits
+  tokens = torch.argmax(logits, axis=-1)
+  text = tokenizer.batch_decode(tokens) # convert tokens to string
+
+  print("text: %s" % text)
+
+#audio_file = path.join(path.dirname(path.realpath(__file__)), "data/136FinalFinal.wav")
+#with sr.AudioFile(audio_file) as source:
+#  while True:
+#    #audio = r.record(source)
+#    print("Going to listen")
+#    audio = r.listen(source)
+#
+#    data = io.BytesIO(audio.get_wav_data()) # list of bytes
+#    clip = AudioSegment.from_file(data) # numpy array
+#    x = torch.FloatTensor(clip.get_array_of_samples()) # tensor
+#
+#    inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt', padding='longest').input_values
+#    logits = model(inputs).logits
+#    tokens = torch.argmax(logits, axis=-1)
+#    text = tokenizer.batch_decode(tokens) # convert tokens to string
+#
+#    print("text: %s" % text)
