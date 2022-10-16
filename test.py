96eb61fe7ef01359076d562ed6b4963192b88392
Eric Tang <github-public@erictang.com>
2022-10-16 20:10:00 +0100

    Read from one single audio file and segment it before putting it through wav2vec2

--- test.py
+++ test.py
@@ -15,18 +15,16 @@ model = Wav2Vec2ForCTC.from_pretrained(MODEL)
 
 r = sr.Recognizer()
 print("Loading file")
-audio_file = AudioSegment.from_wav("data/test_long.wav")
-chunks = split_on_silence(audio_file, min_silence_len=300, silence_thresh=-16)
-print(type(chunks))
-print(type(chunks[0]))
-print(len(chunks))
+audio_file = AudioSegment.from_wav("data/test_long.44100.wav")
+chunks = split_on_silence(audio_file, min_silence_len=300, silence_thresh=-40, keep_silence=True) # keep the silence so that we can calculate the offset of a chunk base on the length of previosu chunks
 
+offset=0.0
 i = 0
 for chunk in chunks:
-  chunk.export("temp/%04d.mp3" % i, format="mp3")
   i += 1
+  #chunk.export("temp/%04d.mp3" % i, format="mp3")
 
-  clip = chunk # numpy array
+  clip = chunk.set_frame_rate(16000) # numpy array
   x = torch.FloatTensor(clip.get_array_of_samples()) # tensor
 
   inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt', padding='longest').input_values
@@ -34,7 +32,8 @@ for chunk in chunks:
   tokens = torch.argmax(logits, axis=-1)
   text = tokenizer.batch_decode(tokens) # convert tokens to string
 
-  print("text: %s" % text)
+  print("text[%04d, %.4f]: %s" % (i, offset/1000.0, text))
+  offset += len(chunk)
 
 #audio_file = path.join(path.dirname(path.realpath(__file__)), "data/136FinalFinal.wav")
 #with sr.AudioFile(audio_file) as source:
