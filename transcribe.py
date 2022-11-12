0f71b5d808c1e285ae9f563f535af92c8cf4c984
Eric Tang <github-public@erictang.com>
2022-11-12 11:16:16 +0000

    Use attention mask in model, also only load CPU model if necessary

--- transcribe.py
+++ transcribe.py
@@ -37,7 +37,18 @@ MAX_CHUNK_LENGTH_MS_FOR_GPU=25000 # if the chunk is longer than this, then use t
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 MODEL_DTYPE_GPU = torch.HalfTensor
 MODEL_DTYPE_CPU = torch.FloatTensor # some operation (eg convultion) in HalfTensor is not supported on CPU
-SHOULD_SHOW_GPU = False
+SHOULD_SHOW_GPU = True
+
+MODEL_CACHE = {}
+def get_model(use_gpu):
+  if not use_gpu in MODEL_CACHE:
+    if use_gpu:
+      log("Loading model to GPU")
+      MODEL_CACHE[use_gpu] = Wav2Vec2ForCTC.from_pretrained(MODEL).type(MODEL_DTYPE_GPU).to(DEVICE)
+    else:
+      log("Loading model to CPU")
+      MODEL_CACHE[use_gpu] = Wav2Vec2ForCTC.from_pretrained(MODEL)
+  return MODEL_CACHE[use_gpu]
 
 def join_short_chunks(in_chunks, min_length_ms):
   temp_chunks = list(in_chunks)
@@ -60,10 +71,6 @@ def join_short_chunks(in_chunks, min_length_ms):
 def transcribe(in_path, default_silence_threshold):
   log("Loading tokenizer")
   tokenizer = Wav2Vec2Processor.from_pretrained(MODEL)
-  log("Loading model to GPU")
-  model_gpu = Wav2Vec2ForCTC.from_pretrained(MODEL).type(MODEL_DTYPE_GPU).to(DEVICE)
-  log("Loading model to CPU")
-  model_cpu = Wav2Vec2ForCTC.from_pretrained(MODEL).type(MODEL_DTYPE_CPU)
 
   #recognizer = sr.Recognizer()
   log("Loading file")
@@ -71,6 +78,7 @@ def transcribe(in_path, default_silence_threshold):
   log("input file -       length: %ds, frame rate: %d, sample_width: %dbits, channels: %d" % (len(audio_file)//1000, audio_file.frame_rate, audio_file.sample_width * 8, audio_file.channels))
   audio_file = audio_file.set_channels(1).set_frame_rate(SAMPLING_RATE)
   log("after resampling - length: %ds, frame rate: %d, sample_width: %dbits, channels: %d" % (len(audio_file)//1000, audio_file.frame_rate, audio_file.sample_width * 8, audio_file.channels))
+
   log("Split file based on silence")
   chunks = split_on_silence(audio_file, min_silence_len=40, silence_thresh=-40, keep_silence=True) # keep the silence so that we can calculate the offset of a chunk base on the length of previosu chunks
   log("Chunk count: %d" % len(chunks))
@@ -78,17 +86,18 @@ def transcribe(in_path, default_silence_threshold):
   chunks = join_short_chunks(chunks, MIN_CHUNK_LENGTH_MS)
   log("Chunk count: %d" % len(chunks))
 
+  #chunks = [audio_file[:i*1000] for i in range(15,45)] # test how long the input can the GPU take
+
   offset_ms=0.0
   chunk_index = 0
   output = []
   for chunk_index in tqdm.tqdm(range(len(chunks)), desc=path.basename(in_path)):
     chunk = chunks[chunk_index]
-    #log("processing chunk %d/%d, offset: %.02fs" % (chunk_index, len(chunks), offset_ms/1000))
+    #log("processing chunk %d/%d, offset: %.02fs, duration: %.02fs" % (chunk_index, len(chunks), offset_ms/1000, len(chunk)/1000))
     #chunk.export("temp/%04d.mp3" % i, format="mp3")
     use_gpu = (len(chunk) <= MAX_CHUNK_LENGTH_MS_FOR_GPU)
-    #log("use gpu: %s" % use_gpu)
     if not use_gpu:
-      log("Using CPU to process chunk, i: %d, len: %.02fs" % (chunk_index, len(chunk)))
+      log("Using CPU to process chunk, i: %d, len: %.02fs, offset: %.02fs" % (chunk_index, len(chunk)/1000, offset_ms/1000))
 
     # convert to tensor
     #log("conveting to Tensor")
@@ -99,35 +108,47 @@ def transcribe(in_path, default_silence_threshold):
     #log("sample tensor (%s) :(%s)" % (x.dtype, list(x.shape)))
 
     #log("tokenizing")
-    inputs = tokenizer(x, sampling_rate=SAMPLING_RATE, return_tensors='pt', padding='longest').input_values
-    if use_gpu:
-      inputs = inputs.type(MODEL_DTYPE_GPU).to(DEVICE)
-    else:
-      inputs = inputs.type(MODEL_DTYPE_CPU)
-    #log("input (%s) length: %d, duration: %.02fs" % (inputs.dtype, inputs.shape[1], len(chunk)/1000))
-    log_gpu()
+    #inputs = tokenizer(x, sampling_rate=SAMPLING_RATE, return_tensors='pt', padding='longest')
+    inputs = tokenizer(x, sampling_rate=SAMPLING_RATE, return_tensors='pt', padding=True)
 
-    #log("getting logits")
+    #log("attention mask - type: %s" % inputs.attention_mask.dtype)
+    model = get_model(use_gpu)
     if use_gpu:
-      logits = model_gpu(inputs).logits
+      #log("getting input_values")
+      input_values = inputs.input_values.type(MODEL_DTYPE_GPU).to(DEVICE)
+      #log_gpu()
+
+      #log("getting attention mask")
+      attention_mask = inputs.attention_mask.type(torch.ShortTensor).to(DEVICE)
+      #log("attention_mask type: %s, dtype: %s, shape: %s" % (type(attention_mask), attention_mask.dtype, list(attention_mask.shape)))
+      #log_gpu()
+
+      #log("getting logits")
+      logits = model(input_values, attention_mask=attention_mask).logits
+      #log_gpu()
+      del input_values
+      del attention_mask
+      del inputs
+      #torch.cuda.empty_cache()
     else:
-      logits = model_cpu(inputs).logits
-    log_gpu()
-    del inputs
-    #torch.cuda.empty_cache()
+      input_values = inputs.input_values
+      attention_mask = inputs.attention_mask
+      #log("getting logits")
+      logits = model(input_values, attention_mask=attention_mask).logits
     #log("logits (%s, %s): %s" % (logits.dtype, logits.get_device(), list(logits.shape)))
-    log_gpu()
+    #log_gpu()
     #log("argmax logits")
     tokens = torch.argmax(logits, axis=-1)
-    log_gpu()
+    #log_gpu()
     del logits
     #torch.cuda.empty_cache()
     #log("tokens (%s, %s): %s" % (tokens.dtype, tokens.get_device(),  list(tokens.shape)))
-    log_gpu()
+    #log_gpu()
     #log("decoding tokens")
     text = tokenizer.batch_decode(tokens)[0] # convert tokens to string
     del tokens 
     #torch.cuda.empty_cache()
+    #log_gpu()
 
 
     #print("%04d,%s,%s" % (chunk_index, format_time_from_sec(offset_ms/1000.0), text))
